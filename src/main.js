2c48213e399cfc8ccc0eaacf2ebcc72c866b070d
Jean Sarlôn <jean.sarlon@icloud.com>
2019-03-01 11:41:01 -0300

    test coverage

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@
 const fizz = 'Fizz';
 const buzz = 'Buzz';
 const FizzBuzz = (n) => {
+  if (!n) return 0;
   if (n % 3 === 0 && n % 5 === 0) return fizz + buzz;
   if (n % 3 === 0) return fizz;
   if (n % 5 === 0) return buzz;
 
   return n;
 };
+const bla = () => 'bla';
+bla();
 
 export default FizzBuzz;
